42f2d59a25da9f9b53ced93f7ff6ab59c8737ac7
Tommy Persaud <tpersaud27@gmail.com>
2022-10-11 17:16:53 -0400

    Update

---
+++ Client/src/app/members/member-lists/member-lists.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-member-lists',
+  templateUrl: './member-lists.component.html',
+  styleUrls: ['./member-lists.component.css']
+})
+export class MemberListsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
