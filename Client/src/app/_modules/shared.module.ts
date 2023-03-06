35bbce6ab611cba4225e7fc485d9044b45d31f11
Tommy Persaud <tpersaud27@gmail.com>
2023-03-06 17:27:24 -0500

    Photo Uploader Configuration

--- Client/src/app/_modules/shared.module.ts
+++ Client/src/app/_modules/shared.module.ts
@@ -5,6 +5,7 @@ import { ToastrModule } from 'ngx-toastr';
 import { TabsModule } from 'ngx-bootstrap/tabs';
 import { NgxGalleryModule } from '@kolkov/ngx-gallery';
 import { NgxSpinnerModule } from 'ngx-spinner';
+import { FileUploadModule } from 'ng2-file-upload';
 
 @NgModule({
   declarations: [],
@@ -19,6 +20,7 @@ import { NgxSpinnerModule } from 'ngx-spinner';
     NgxSpinnerModule.forRoot({
       type: 'line-scale-party',
     }),
+    FileUploadModule,
   ],
   exports: [
     BsDropdownModule,
@@ -26,6 +28,7 @@ import { NgxSpinnerModule } from 'ngx-spinner';
     TabsModule,
     NgxGalleryModule,
     NgxSpinnerModule,
+    FileUploadModule,
   ],
 })
 export class SharedModule {}
--- Client/src/app/members/photo-editor/photo-editor.component.ts
+++ Client/src/app/members/photo-editor/photo-editor.component.ts
@@ -1,5 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { take } from 'rxjs/operators';
 import { Member } from 'src/app/_models/Member';
+import { User } from 'src/app/_models/User';
+import { AccountService } from 'src/app/_services/account.service';
+import { environment } from 'src/environments/environment';
+import { FileUploader, FileItem, FileUploaderOptions } from 'ng2-file-upload';
 
 @Component({
   selector: 'app-photo-editor',
@@ -11,7 +16,54 @@ export class PhotoEditorComponent implements OnInit {
   // Parent is the member edit component
   @Input() member: Member | undefined;
 
-  constructor() {}
+  // Ng2 File Uploader config
+  uploader: FileUploader | undefined;
+  hasBaseDropZoneOver = false;
 
-  ngOnInit(): void {}
+  // We need access to the api so we can uploading the image
+  baseUrl = environment.apiUrl;
+  // We need to user
+  user: User | undefined;
+
+  constructor(private accountService: AccountService) {
+    // Getting the user
+    this.accountService.currentUser$.pipe(take(1)).subscribe({
+      next: (user) => {
+        if (user) this.user = user;
+      },
+    });
+  }
+
+  ngOnInit(): void {
+    this.initializeUploader();
+  }
+
+  fileOverBase(event: any) {
+    this.hasBaseDropZoneOver = event;
+  }
+
+  // Giving the file uploader some initial configurations
+  initializeUploader() {
+    this.uploader = new FileUploader({
+      // Note: Since we are outside of the http request we must also specify the JWT Token in the header
+      url: this.baseUrl + 'users/add-photo',
+      authToken: 'Bearer ' + this.user?.token,
+      isHTML5: true,
+      allowedFileType: ['image'],
+      removeAfterUpload: true,
+      autoUpload: false,
+      maxFileSize: 10 * 1024 * 1024,
+    });
+
+    this.uploader.onAfterAddingFile = (file) => {
+      file.withCredentials = false;
+    };
+
+    this.uploader.onSuccessItem = (item, response, status, headers) => {
+      if (response) {
+        const photo = JSON.parse(response);
+        this.member.photos.push(photo);
+      }
+    };
+  }
 }
