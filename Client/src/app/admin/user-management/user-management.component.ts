897f6167c4064b3e58e5b7b4aade31b629a04314
Tommy Persaud <68301047+tpersaud27@users.noreply.github.com>
2023-04-30 20:08:17 -0400

    Added admin edit roles component
    Display a list of users and their roles

---
+++ Client/src/app/admin/user-management/user-management.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/_models/User';
+import { AdminService } from 'src/app/_services/admin.service';
+
+@Component({
+  selector: 'app-user-management',
+  templateUrl: './user-management.component.html',
+  styleUrls: ['./user-management.component.css'],
+})
+export class UserManagementComponent implements OnInit {
+  users: User[] | User = [];
+
+  constructor(private adminService: AdminService) {}
+
+  ngOnInit(): void {
+    this.getUsersWithRoles();
+  }
+
+  getUsersWithRoles() {
+    this.adminService.getUsersWithRoles().subscribe({
+      next: (users) => (this.users = users),
+    });
+  }
+}
--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -28,6 +28,8 @@ import { DatePickerComponent } from './_forms/date-picker/date-picker.component'
 import { MemberMessagesComponent } from './members/member-messages/member-messages.component';
 import { AdminPanelComponent } from './admin/admin-panel/admin-panel.component';
 import { HasRoleDirective } from './_directives/has-role.directive';
+import { UserManagementComponent } from './admin/user-management/user-management.component';
+import { PhotoManagementComponent } from './admin/photo-management/photo-management.component';
 
 @NgModule({
   declarations: [
@@ -50,6 +52,8 @@ import { HasRoleDirective } from './_directives/has-role.directive';
     MemberMessagesComponent,
     AdminPanelComponent,
     HasRoleDirective,
+    UserManagementComponent,
+    PhotoManagementComponent,
   ],
   imports: [
     BrowserModule,
