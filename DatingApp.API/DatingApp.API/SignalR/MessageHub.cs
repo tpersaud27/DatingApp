4cfcff4f1e9823147177a1dc0ed4c325796d9e02
Tommy Persaud <68301047+tpersaud27@users.noreply.github.com>
2023-05-26 11:18:29 -0400

    send message method

--- DatingApp.API/DatingApp.API/SignalR/MessageHub.cs
+++ DatingApp.API/DatingApp.API/SignalR/MessageHub.cs
@@ -1,4 +1,8 @@
-﻿using DatingApp.API.Interfaces;
+﻿using AutoMapper;
+using DatingApp.API.DTOs;
+using DatingApp.API.Entities;
+using DatingApp.API.Interfaces;
+using DatingApp.DAL.Interfaces;
 using DatingApp.Services.Extensions;
 using Microsoft.AspNetCore.SignalR;
 
@@ -7,12 +11,14 @@ namespace DatingApp.API.SignalR
     public class MessageHub: Hub
     {
         private readonly IMessageRepository _messageRepository;
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
 
-        public MessageHub(IMessageRepository messageRepository) { 
+        public MessageHub(IMessageRepository messageRepository, IUserRepository userRepository, IMapper mapper) { 
             
             _messageRepository = messageRepository;
-                
-        
+            _userRepository = userRepository;
+            _mapper = mapper;
         }
 
         public override async Task OnConnectedAsync()
@@ -37,6 +43,50 @@ namespace DatingApp.API.SignalR
         public override Task OnDisconnectedAsync(Exception exception)
         {
             return base.OnDisconnectedAsync(exception);
+        }
+
+        public async Task SendMessage(CreateMessageDto createMessageDto)
+        {
+              // Get the username from the claism
+            var username  = Context.User.GetUsername();
+            // Check if the person is trying to send a message to themselves
+            if(username == createMessageDto.RecipientUsername.ToLower())
+            {
+                // Note: Exceptions are more expensive on our server as compared to http responses
+                // since we are not in a controller we have to throw an exception here
+                throw new HubException("You cannot send messages to your self");
+            }
+            // Get the user entity
+            var sender = await _userRepository.GetUserByUsernameAsync(username);
+            // Get the recipient entity
+            var recipient = await _userRepository.GetUserByUsernameAsync(createMessageDto.RecipientUsername);
+
+            // Check if the recipient the user is trying to send a message to exists
+            if(recipient== null)
+            {
+                throw new HubException("Not found user");
+            }
+
+            // Create out new message entity
+            var message = new Message
+            {
+                Sender = sender,
+                Recipient = recipient,
+                SenderUserName = sender.UserName,
+                RecipientUsername = recipient.UserName,
+                SenderId = sender.Id,
+                RecipientId = recipient.Id,
+                Content = createMessageDto.Content,
+            };
+            // Add the message
+            _messageRepository.AddMessage(message);
+
+            if(await _messageRepository.SaveAllAsync())
+            {
+                var group = GetGroupName(sender.UserName, recipient.UserName);
+                await Clients.Group(group).SendAsync("NewMessage", _mapper.Map<MessageDto>(message));
+            }
+
         }
 
 
