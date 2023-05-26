6cbbc2d81e8044413cec3b9d1647bdac94ad81df
Tommy Persaud <tpersaud27@gmail.com>
2023-05-26 07:50:01 -0400

    Message hub file creation
    Just updating github with latest.

---
+++ DatingApp.API/DatingApp.API/SignalR/MessageHub.cs
@@ -0,0 +1,20 @@
+﻿using DatingApp.API.Interfaces;
+using Microsoft.AspNetCore.SignalR;
+
+namespace DatingApp.API.SignalR
+{
+    public class MessageHub: Hub
+    {
+
+        public MessageHub(IMessageRepository messageRepository) { 
+        
+        
+        
+        
+        
+        }
+
+
+
+    }
+}
